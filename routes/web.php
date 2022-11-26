646cc294c23e65ea517d19228965afd2eeb9670e
nealpatron <99371910+nealpatron@users.noreply.github.com>
2022-11-25 18:34:44 -0600

    Copied from tutorial

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+*/
+
+Route::get('{any}', function () {
+    return view('app');
+})->where('any', '.*');
