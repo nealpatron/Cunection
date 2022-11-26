8c04e05ede4b20ad5a6a12c76fc8edff749e3ce9
nealpatron <99371910+nealpatron@users.noreply.github.com>
2022-11-25 19:17:24 -0600

    css works now

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" value="{{ csrf_token() }}"/>
     <title>{{env('APP_NAME')}}</title>
-    <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet"/>
+    @vite(['resources/sass/app.scss'])
 </head>
 <body>
 
@@ -29,6 +29,6 @@
 
 <div id="app">
 </div>
-<script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
+@vite(['resources/js/app.js'])
 </body>
 </html>
