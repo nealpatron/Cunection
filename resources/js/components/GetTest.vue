8f7f27df70f55a7ba75b1a4f5b5f66760261ac50
Faith Brown <faithbrown55@outlook.com>
2022-11-25 21:39:22 -0600

    Added get test page

---
+++ resources/js/components/GetTest.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    data() {
+        return {
+            info: [],
+        };
+    },
+    methods: {
+        getData() {
+            this.$axios.get("/sanctum/csrf-cookie").then((response) => {
+                this.$axios
+                    .get("/api/books") // change endpoint here 
+                    .then((response) => {
+                        this.info = response.data;
+                    })
+                    .catch(function (error) {
+                        console.error(error);
+                    });
+            });
+        },
+    },
+};
+</script>
+
+<template>
+    <div>
+        <button @click="getData">Get Data</button>
+        <p>{{ info }}</p>
+    </div>
+</template>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,57 +1,84 @@
-import {createWebHistory, createRouter} from "vue-router";
-
-
-import Home from '../pages/Home.vue';
-import About from '../pages/About.vue';
-import Register from '../pages/Register.vue';
-import Login from '../pages/Login.vue';
-import Dashboard from '../pages/Dashboard.vue';
-
-import Books from '../components/Books.vue';
-import AddBook from '../components/AddBook.vue';
-import EditBook from '../components/EditBook.vue';
+import { createWebHistory, createRouter } from "vue-router";
 
 export const routes = [
     {
-        name: 'home',
-        path: '/',
-        component: Home
+        name: "home",
+        path: "/",
+        component: () => import("../pages/Home.vue"),
     },
     {
-        name: 'about',
-        path: '/about',
-        component: About
+        name: "register",
+        path: "/register",
+        component: () => import("../pages/Register.vue"),
     },
     {
-        name: 'register',
-        path: '/register',
-        component: Register
+        name: "login",
+        path: "/login",
+        component: () => import("../pages/Login.vue"),
     },
     {
-        name: 'login',
-        path: '/login',
-        component: Login
+        name: "dashboard",
+        path: "/dashboard",
+        component: () => import("../pages/Dashboard.vue"),
     },
     {
-        name: 'dashboard',
-        path: '/dashboard',
-        component: Dashboard
+        name: "books",
+        path: "/books",
+        component: () => import("../components/Books.vue"),
     },
     {
-        name: 'books',
-        path: '/books',
-        component: Books
+        name: "addbook",
+        path: "/books/add",
+        component: () => import("../components/AddBook.vue"),
     },
     {
-        name: 'addbook',
-        path: '/books/add',
-        component: AddBook
+        name: "editbook",
+        path: "/books/edit/:id",
+        component: () => import("../components/EditBook.vue"),
     },
     {
-        name: 'editbook',
-        path: '/books/edit/:id',
-        component: EditBook
+        name: "gettest",
+
+        path: "/gettest",
+        component: () => import("../components/Gettest.vue"),
     },
+    /*
+    {
+        path: "/events-follow-list",
+        name: "events-follow-list",
+        component: () => import("../views/events/FollowListView.vue"),
+      },
+      {
+        path: "/events-follow-calendar",
+        name: "events-follow-calendar",
+        component: () => import("../views/events/FollowCalendarView.vue"),
+      },
+      {
+        path: "/events-all-list",
+        name: "events-all-list",
+        component: () => import("../views/events/AllListView.vue"),
+      },
+      {
+        path: "/events-all-calendar",
+        name: "events-all-calendar",
+        component: () => import("../views/events/AllCalendarView.vue"),
+      },
+      {
+        path: "/clubs-search",
+        name: "search",
+        component: () => import("../views/clubs/SearchView.vue"),
+      },
+      {
+        path: "/clubs-follow",
+        name: "clubs-follow",
+        component: () => import("../views/clubs/FollowedClubsView.vue"),
+      },
+      {
+        path: "/clubs-browse",
+        name: "clubs-browse",
+        component: () => import("../views/clubs/BrowseClubsView.vue"),
+      },
+      */
 ];
 
 const router = createRouter({
