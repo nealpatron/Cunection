931312cdb96d209f45ccb038a313bed89a0ad242
Faith Brown <faithbrown55@outlook.com>
2022-11-30 12:09:10 -0600

    Added styling to club cards

--- resources/js/pages/clubs/BrowseClubsView.vue
+++ resources/js/pages/clubs/BrowseClubsView.vue
@@ -4,6 +4,7 @@ export default {
     data() {
         return {
             items: [],
+            isFollowing: false,
         };
     },
     mounted() {
@@ -27,12 +28,34 @@ export default {
 </script>
 
 <template>
-    <v-list lines="two">
-        <v-list-item
-            v-for="item in items"
-            :key="item.id"
-            :title="item.name"
-            :subtitle="item.description"
-        ></v-list-item>
-    </v-list>
+    <p class="text-h3 ml-7 mt-5 text-decoration-underline">
+        Available Clubs on Campus
+    </p>
+    <div class="d-flex align-center mx-10 mt-12 flex-column">
+        <v-row class="justify-space-between">
+            <v-card
+                v-for="item in items"
+                :key="item.id"
+                :title="item.name"
+                :text="item.description"
+                color="#C8C8C8"
+                theme="light"
+                class="my-2 elevation-3"
+                width="500"
+            >
+                <v-card-actions>
+                    <div v-if="isFollowing">
+                        <v-btn color="#646464" theme="dark" variant="flat" size="small"
+                            >Unfollow</v-btn
+                        >
+                    </div>
+                    <div v-else>
+                        <v-btn color="#5A9DBF" theme="dark" variant="flat" size="small"
+                            >Follow</v-btn
+                        >
+                    </div>
+                </v-card-actions>
+            </v-card>
+        </v-row>
+    </div>
 </template>
