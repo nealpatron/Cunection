0b370edd865752c9f940435d2bcfad3729b3d932
Faith Brown <faithbrown55@outlook.com>
2022-11-29 22:40:01 -0600

    Added followed clubs

--- resources/js/pages/clubs/FollowedClubsView.vue
+++ resources/js/pages/clubs/FollowedClubsView.vue
@@ -1,11 +1,38 @@
 <script>
 export default {
-  name: "FollowedClubs",
+    name: "FollowedClubs",
+    data() {
+        return {
+            items: [],
+        };
+    },
+    mounted() {
+        this.getAllClubs();
+    },
+    methods: {
+        getFollowedClubs() {
+            this.$axios.get("/sanctum/csrf-cookie").then((response) => {
+                this.$axios
+                    .get("/api/users/me/following")
+                    .then((response) => {
+                        this.items = response.data;
+                    })
+                    .catch(function (error) {
+                        console.error(error);
+                    });
+            });
+        },
+    },
 };
 </script>
 
 <template>
-  <h3>Follow some clubs to see some!</h3>
+    <v-list lines="two">
+        <v-list-item
+            v-for="item in items"
+            :key="item.id"
+            :title="item.name"
+            :subtitle="item.description"
+        ></v-list-item>
+    </v-list>
 </template>
-
-<style scoped></style>
