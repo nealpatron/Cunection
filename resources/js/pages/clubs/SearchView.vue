3396e0c9c2142d8b042a9a2928a24d57c7f16526
Faith Brown <faithbrown55@outlook.com>
2022-12-01 21:48:49 -0600

    Moved search bar to search page

--- resources/js/pages/clubs/SearchView.vue
+++ resources/js/pages/clubs/SearchView.vue
@@ -1,22 +1,38 @@
 <script>
-import SearchBar from "../../components/SearchBar.vue";
-
 export default {
-  name: "Search",
-  components: {
-    SearchBar,
-  },
+    name: "Search",
+    data() {
+        return {
+            input: "",
+            clubs: [],
+        };
+    },
+    mounted() {
+        this.getAllClubs;
+    },
+    methods: {
+        getAllClubs() {
+            this.$axios.get("/sanctum/csrf-cookie").then((response) => {
+                this.$axios
+                    .get("/api/clubs")
+                    .then((response) => {
+                        this.clubs = response.data;
+                    })
+                    .catch(function (error) {
+                        console.error(error);
+                    });
+            });
+        },
+    },
 };
 </script>
 
 <template>
-  <div class="container">
-    <SearchBar />
-  </div>
+    <p class="text-h3 ml-7 mt-5 text-decoration-underline">
+        Looking for a Club?
+    </p>
+    <!-- Doesn't work -->
+    <v-col cols="5">
+        <v-combobox class="mt-10" clearable label="Search..." :items="clubs" />
+    </v-col>
 </template>
-
-<style>
-.container{
-    margin: 20px;
-}
-</style>
